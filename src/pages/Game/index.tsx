c2fc7994c054bb4eb4e94a74d46ad1368114566d
Hillel1408 <etemax@bk.ru>
2023-12-02 17:27:11 +0300

    Пятнашки РТ

--- src/pages/Game/index.tsx
+++ src/pages/Game/index.tsx
@@ -11,8 +11,6 @@ function Game() {
 
     const userData = useAppSelector((state) => state.main.userData);
 
-    !userData.user_id && navigate(ROUTES.HOME);
-
     const expiryTimestamp = new Date();
     expiryTimestamp.setSeconds(expiryTimestamp.getSeconds() + 180);
 
@@ -81,128 +79,134 @@ function Game() {
 
     return (
         <>
-            <div className="container">
-                <div className="py-[73px] flex justify-between items-center">
-                    <Logo />
+            {userData.user_id ? (
+                <div>
+                    <div className="container">
+                        <div className="py-[73px] flex justify-between items-center">
+                            <Logo />
 
-                    {!win && (
-                        <div className="bg-[#F54D0D] text-white text-[80px] font-bold px-6">
-                            {minutes}:{seconds}
-                        </div>
-                    )}
-                </div>
-
-                <div className="bg-white rounded-t-[100px] pt-[107px] pb-[80px] px-[141px] flex flex-col items-center gap-[10px] min-h-[1597px]">
-                    <div className="relative w-full text-center">
-                        <h1
-                            className={classNames(
-                                "text-[80px] font-bold",
-                                userData.id === "puzzle_3x3" ? "mb-[235px]" : "mb-[329px]",
-                                win
-                                    ? "text-[#00B23C]"
-                                    : seconds === 0 && minutes === 0
-                                    ? "text-[#F40A0A]"
-                                    : "text-[#000]",
+                            {!win && (
+                                <div className="bg-[#F54D0D] text-white text-[80px] font-bold px-6">
+                                    {minutes}:{seconds}
+                                </div>
                             )}
-                        >
-                            {win
-                                ? "Победа!"
-                                : seconds === 0 && minutes === 0
-                                ? "Время вышло"
-                                : "Соберите картинку"}
-                        </h1>
+                        </div>
 
-                        <p className="absolute top-[130px] text-[36px] text-[#000] font-bold left-0 right-0">
-                            {win
-                                ? "Поздравляем, %username%! Вы верно собрали картинку"
-                                : seconds === 0 &&
-                                  minutes === 0 &&
-                                  "В следующий раз, попробуйте действовать быстрее"}
-                        </p>
-                    </div>
+                        <div className="bg-white rounded-t-[100px] pt-[107px] pb-[80px] px-[141px] flex flex-col items-center gap-[10px] min-h-[1597px]">
+                            <div className="relative w-full text-center">
+                                <h1
+                                    className={classNames(
+                                        "text-[80px] font-bold",
+                                        userData.id === "puzzle_3x3" ? "mb-[235px]" : "mb-[329px]",
+                                        win
+                                            ? "text-[#00B23C]"
+                                            : seconds === 0 && minutes === 0
+                                            ? "text-[#F40A0A]"
+                                            : "text-[#000]",
+                                    )}
+                                >
+                                    {win
+                                        ? "Победа!"
+                                        : seconds === 0 && minutes === 0
+                                        ? "Время вышло"
+                                        : "Соберите картинку"}
+                                </h1>
 
-                    <div
-                        className={classNames(
-                            "grid grid-cols-[1fr_1fr_1fr] mb-[226px] border-[0.5px] border-[#000]",
-                            win && "outline outline-8 outline-[#00B23C] rounded-[4px]",
-                            seconds === 0 &&
-                                minutes === 0 &&
-                                "outline outline-8 outline-[#F40A0A] rounded-[4px]",
-                        )}
-                    >
-                        {arr &&
-                            arr.map((array, i) =>
-                                array.map((item, j) => (
-                                    <div
-                                        key={item}
-                                        className={classNames(
-                                            "w-[268px] h-[268px] border-[0.5px] border-[#000]",
-                                        )}
-                                        onClick={() => {
-                                            !win &&
-                                                !(seconds === 0 && minutes === 0) &&
-                                                correctArr(i, j);
+                                <p className="absolute top-[130px] text-[36px] text-[#000] font-bold left-0 right-0">
+                                    {win
+                                        ? "Поздравляем, %username%! Вы верно собрали картинку"
+                                        : seconds === 0 &&
+                                          minutes === 0 &&
+                                          "В следующий раз, попробуйте действовать быстрее"}
+                                </p>
+                            </div>
+
+                            <div
+                                className={classNames(
+                                    "grid grid-cols-[1fr_1fr_1fr] mb-[226px] border-[0.5px] border-[#000]",
+                                    win && "outline outline-8 outline-[#00B23C] rounded-[4px]",
+                                    seconds === 0 &&
+                                        minutes === 0 &&
+                                        "outline outline-8 outline-[#F40A0A] rounded-[4px]",
+                                )}
+                            >
+                                {arr &&
+                                    arr.map((array, i) =>
+                                        array.map((item, j) => (
+                                            <div
+                                                key={item}
+                                                className={classNames(
+                                                    "w-[268px] h-[268px] border-[0.5px] border-[#000]",
+                                                )}
+                                                onClick={() => {
+                                                    !win &&
+                                                        !(seconds === 0 && minutes === 0) &&
+                                                        correctArr(i, j);
+                                                }}
+                                            >
+                                                {(item !== number.current || win) && (
+                                                    <img
+                                                        src={`/images/${image.current}/${userData.id}/${item}.jpeg`}
+                                                        alt=""
+                                                    />
+                                                )}
+                                            </div>
+                                        )),
+                                    )}
+                            </div>
+
+                            {win ? (
+                                <Button text="ПОЛУЧИТЬ КУПОНЫ" className="w-[802px] bg-[#00B23C]" />
+                            ) : seconds === 0 && minutes === 0 ? (
+                                <Button
+                                    text="ЗАВЕРШИТЬ ИГРУ"
+                                    className="w-[802px] bg-[#F40A0A]"
+                                    clickHandler={() => {
+                                        navigate(ROUTES.HOME);
+                                    }}
+                                />
+                            ) : (
+                                <div className="flex justify-between w-full">
+                                    <SecondaryButton
+                                        text="Смотреть подсказку"
+                                        className="text-[#8D1BFF]"
+                                        clickHandler={() => {
+                                            setActive(true);
                                         }}
                                     >
-                                        {(item !== number.current || win) && (
-                                            <img
-                                                src={`/images/${image.current}/${userData.id}/${item}.jpeg`}
-                                                alt=""
-                                            />
-                                        )}
-                                    </div>
-                                )),
+                                        <svg className="h-[33px] w-[33px]" aria-hidden="true">
+                                            <use xlinkHref="/sprites/sprite.svg#help"></use>
+                                        </svg>
+                                    </SecondaryButton>
+
+                                    <SecondaryButton
+                                        text="Завершить игру"
+                                        className="text-[#F40A0A]"
+                                        clickHandler={() => {
+                                            setActiveModal(true);
+                                        }}
+                                    >
+                                        <svg className="h-[33px] w-[33px]" aria-hidden="true">
+                                            <use xlinkHref="/sprites/sprite.svg#finish"></use>
+                                        </svg>
+                                    </SecondaryButton>
+                                </div>
                             )}
+                        </div>
                     </div>
 
-                    {win ? (
-                        <Button text="ПОЛУЧИТЬ КУПОНЫ" className="w-[802px] bg-[#00B23C]" />
-                    ) : seconds === 0 && minutes === 0 ? (
-                        <Button
-                            text="ЗАВЕРШИТЬ ИГРУ"
-                            className="w-[802px] bg-[#F40A0A]"
-                            clickHandler={() => {
-                                navigate(ROUTES.HOME);
-                            }}
-                        />
-                    ) : (
-                        <div className="flex justify-between w-full">
-                            <SecondaryButton
-                                text="Смотреть подсказку"
-                                className="text-[#8D1BFF]"
-                                clickHandler={() => {
-                                    setActive(true);
-                                }}
-                            >
-                                <svg className="h-[33px] w-[33px]" aria-hidden="true">
-                                    <use xlinkHref="/sprites/sprite.svg#help"></use>
-                                </svg>
-                            </SecondaryButton>
+                    <ImageModal
+                        active={active}
+                        setActive={setActive}
+                        image={image.current}
+                        difficulty={userData.id}
+                    />
 
-                            <SecondaryButton
-                                text="Завершить игру"
-                                className="text-[#F40A0A]"
-                                clickHandler={() => {
-                                    setActiveModal(true);
-                                }}
-                            >
-                                <svg className="h-[33px] w-[33px]" aria-hidden="true">
-                                    <use xlinkHref="/sprites/sprite.svg#finish"></use>
-                                </svg>
-                            </SecondaryButton>
-                        </div>
-                    )}
+                    <FinishModal active={activeModal} setActive={setActiveModal} />
                 </div>
-            </div>
-
-            <ImageModal
-                active={active}
-                setActive={setActive}
-                image={image.current}
-                difficulty={userData.id}
-            />
-
-            <FinishModal active={activeModal} setActive={setActiveModal} />
+            ) : (
+                navigate(ROUTES.HOME)
+            )}
         </>
     );
 }
