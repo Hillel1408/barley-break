8056fb93f18041ec8a669a2b883a37a53f2c3e16
Hillel1408 <etemax@bk.ru>
2023-12-05 11:49:25 +0300

    Пятнашки РТ

--- src/pages/Game/index.tsx
+++ src/pages/Game/index.tsx
@@ -146,7 +146,7 @@ function Game() {
                                             >
                                                 {(item !== number.current || win) && (
                                                     <img
-                                                        src={`/images/${image.current}/${userData.id}/${item}.jpeg`}
+                                                        src={`/images/${image.current}/${userData.id}/${item}.webp`}
                                                         alt=""
                                                     />
                                                 )}
